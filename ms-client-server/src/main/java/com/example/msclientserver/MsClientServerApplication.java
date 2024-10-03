c3ca3db7aec2c5ae3f9f9f8bbea60da1da6dfddd
kiohunter12 <kioarce12@gmail.com>
2024-10-02 21:45:52 -0500

    gateway service

---
+++ ms-client-server/src/main/java/com/example/msclientserver/MsClientServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.msclientserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsClientServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsClientServerApplication.class, args);
+    }
+
+}
---
+++ ms-client-server/src/test/java/com/example/msclientserver/MsClientServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msclientserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClientServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
